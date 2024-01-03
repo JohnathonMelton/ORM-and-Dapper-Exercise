855410721232a967c274b83a571a956160c39cfc
Johnathon Melton <seventh.acoustic@gmail.com>
2024-01-03 09:20:41 -0600

    Added classes and interfaces

---
+++ ORM-Dapper/DapperDepartmentRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public class DapperDepartmentRepository : IDepartmentRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public DapperDepartmentRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Department> GetAllDepartments()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
